062faf6ca1056552a94bb297bbab06fb8fe098ed
lorenzomene <lorenzospmene@gmail.com>
2024-09-06 17:12:33 -0300

    feat: create mod and start todo structure

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lorenzomene/go-todo-cli
+
+go 1.23.0
---
+++ todo.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"errors"
+	"time"
+)
+
+type StatusType int
+
+const (
+	TODO StatusType = iota
+	DOING
+	DONE
+)
+
+type Task struct {
+	Title       string
+	Status      StatusType
+	CreatedAt   time.Time
+	CompletedAt *time.Time
+}
+
+type Todos []Task
+
+func (todos *Todos) addTask(title string) {
+	todo := Task{
+		Title:       title,
+		Status:      TODO,
+		CreatedAt:   time.Now(),
+		CompletedAt: nil,
+	}
+
+	*todos = append(*todos, todo)
+}
+
+func (todos *Todos) validateIndex(idx int) error {
+	if idx < 0 || idx >= len(*todos) {
+		err := errors.New("Invalid index")
+		return err
+	}
+	return nil
+}
+
+func (todos *Todos) removeTask() {}
