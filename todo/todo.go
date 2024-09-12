c0bb13a46127c08552d592f250ba9a83ab1c6c50
lorenzomene <lorenzospmene@gmail.com>
2024-09-12 15:10:22 -0300

    feat: read and write to csv file for storage

--- todo/todo.go
+++ todo/todo.go
@@ -1,7 +1,10 @@
 package todo
 
 import (
+	"encoding/csv"
 	"errors"
+	"os"
+	"strconv"
 	"time"
 )
 
@@ -87,6 +90,87 @@ func (todos *Todos) UpdateTitle(idx int, title string) error {
 	}
 
 	t[idx].Title = title
+
+	return nil
+}
+
+func (todos *Todos) SaveToCSV(filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for _, task := range *todos {
+		completedAt := ""
+		if task.CompletedAt != nil {
+			completedAt = task.CompletedAt.Format("02-Jan-2006 15:04")
+		}
+
+		err := writer.Write([]string{
+			task.Title,
+			strconv.Itoa(int(task.Status)),
+			task.CreatedAt.Format("02-Jan-2006 15:04"),
+			completedAt,
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (todos *Todos) LoadFromCSV(filename string) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	records, err := reader.ReadAll()
+	if err != nil {
+		return err
+	}
+
+	for _, record := range records {
+		if len(records) != 4 {
+			return errors.New("invalid record length")
+		}
+
+		statusInt, err := strconv.Atoi(record[1])
+		if err != nil {
+			return err
+		}
+		statusType := StatusType(statusInt)
+
+		createdAt, err := time.Parse("02-Jan-2006 15:04", record[2])
+		if err != nil {
+			return err
+		}
+
+		var completedAt *time.Time
+		if record[3] != "" {
+			t, err := time.Parse("02-Jan-2006 15:04", record[3])
+			if err != nil {
+				return err
+			}
+			completedAt = &t
+		}
+
+		task := Task{
+			Title:       record[0],
+			Status:      statusType,
+			CreatedAt:   createdAt,
+			CompletedAt: completedAt,
+		}
+		*todos = append(*todos, task)
+	}
 
 	return nil
 }
